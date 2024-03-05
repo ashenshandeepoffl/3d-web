9079ebd388c45e7a4b6ff58ca45c3e17f9eda773
ashenshandeepoffl <ashenshandeepoffl@gmail.com>
2024-03-05 06:51:05 +0530

    Updated About Section

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -10,7 +10,7 @@ const Computers = ({ isMobile }) => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={1} groundColor='black' />
+      <hemisphereLight intensity={10} groundColor='black' />
       <pointLight intensity={1} />
       <spotLight
         position={[-20, 50, 10]}
